c4426c3a9648b95a82099ac31909f84191aaf045
matheusbutturi <97640514+matheusbutturi@users.noreply.github.com>
2023-01-18 20:44:22 -0300

    Update aula16-twitter-post.py

--- Twitter_API/aula16-twitter-post.py
+++ Twitter_API/aula16-twitter-post.py
@@ -3,11 +3,11 @@ import json
 import urllib.parse
 
 # Get the consumer and token (key/secret) params to access Twitter API
-consumer_key = 'IH53HhQa7m5p3IAHfO6XDy5Cd'
-consumer_secret = 'y4LllRuAYN1UUI0tQGz5iAdpK3Hufyat05XDjEFvH3LwPC5imV'
+consumer_key = 'xxxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxx'
 
-token_key = '1615807020577218560-Iu71vg1Tdnf6TLMz2YJAvdzwRVpGCf'
-token_secret = 'iUR7PpQCP4sgx2mHMNp3W1DZX81nPjRGWPwCx0bRPKLRQ'
+token_key = 'xxxxxxxxxxxxxxxxxxx'
+token_secret = 'xxxxxxxxxxxxxxxxxxxx'
 
 # Input the objects into consumers and token to have access
 consumer = oauth2.Consumer(consumer_key, consumer_secret)
